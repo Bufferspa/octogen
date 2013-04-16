b20b18ab26fc20232a2a20d3a9c013f53038cf6e
Francisco Arevalo <farevalod@gmail.com>
2013-04-16 09:58:50 -0400

    Generating lists and whitelists

--- octogen.py
+++ octogen.py
@@ -1,27 +1,30 @@
 from optparse import OptionParser
 
 parser = OptionParser()
+parser.add_option("-l", "--lists", action="append", type="str", help="Listas negadas en pass")
+parser.add_option("-w", "--whitelists", action="append", type="str", help="Listas pass")
 parser.add_option("-f", "--file", dest="filename",
-                  help="write report to FILE", metavar="FILE")
-parser.add_option("-q", "--quiet",
-                  action="store_false", dest="verbose", default=True,
-                  help="don't print status messages to stdout")
+                  help="Archivo de salida", metavar="FILE")
 
 (options, args) = parser.parse_args()
 
+lists = options.lists or []
+whitelists = options.whitelists or []
+
 with open('squidGuard.conf', 'w') as f:
-	f.write("dbhome /usr/local/squidGuard/db")
-	f.write("logdir /usr/local/squidGuard/logs")
-	
-
-dest porn {
-        domainlist porn/domains
-        urllist porn/urls
-        }
-
-acl {
-        default {
-                pass !porn all
-                redirect http://localhost/block.html
-        }
- }
+	f.write("dbhome /usr/local/squidGuard/db\n")
+	f.write("logdir /usr/local/squidGuard/logs\n")
+	f.write("\n")
+	for lista in lists+whitelists:
+		f.write("dest "+lista+" {\n")
+		f.write("  domainlist "+lista+"/domains\n")
+		f.write("  urllist "+lista+"/urls\n")
+		f.write("}\n")
+		f.write("\n")
+	f.write("\n")
+	f.write("acl {\n")
+	f.write("  default {\n")
+	f.write("    pass !"+" !".join(lists)+" "+" ".join(whitelists)+" all\n")
+	f.write("    redirect http://localhost/block.html\n")
+	f.write("  }\n")
+	f.write("}\n")
