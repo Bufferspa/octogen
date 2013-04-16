3975f1e34b3c30f8365dddb2d546f3e9c33756b2
Francisco Arevalo <farevalod@gmail.com>
2013-04-16 15:49:45 -0400

    Filename handling

--- octogen.py
+++ octogen.py
@@ -1,17 +1,18 @@
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-l", "--lists", action="append", type="str", help="Listas negadas en pass")
-parser.add_option("-w", "--whitelists", action="append", type="str", help="Listas pass")
-parser.add_option("-f", "--file", dest="filename",
+parser.add_option("-l", "--lists", action="append", type="string", help="Listas negadas en pass")
+parser.add_option("-w", "--whitelists", action="append", type="string", help="Listas pass")
+parser.add_option("-f", "--file", dest="filename", action="store", type="string"
                   help="Archivo de salida", metavar="FILE")
 
 (options, args) = parser.parse_args()
 
 lists = options.lists or []
 whitelists = options.whitelists or []
+filename = options.filename or "squidGuard.conf"
 
-with open('squidGuard.conf', 'w') as f:
+with open(filename, 'w') as f:
 	f.write("dbhome /usr/local/squidGuard/db\n")
 	f.write("logdir /usr/local/squidGuard/logs\n")
 	f.write("\n")
