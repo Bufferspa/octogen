90aea2dd335d749360e4c644383bf1e33667c617
Sebastian Etchegaray <bufferadmin@bufferbox.localdomain>
2013-04-25 19:57:40 -0300

    verificar si existe archivo domains"

--- octogen.py
+++ octogen.py
@@ -1,5 +1,6 @@
 from optparse import OptionParser
 import subprocess
+import os
 parser = OptionParser()
 parser.add_option("-l", "--lists", action="append", type="string", help="Listas negadas en pass")
 parser.add_option("-w", "--whitelists", action="append", type="string", help="Listas pass")
@@ -10,17 +11,20 @@ parser.add_option("-f", "--file", dest="filename", action="store", type="string"
 
 (options, args) = parser.parse_args()
 
-lists = options.lists or []
-whitelists = options.whitelists or []
+directory = options.directory or "/usr/local/squiGuard/db"
 filename = options.filename or "squidGuard.conf"
-directory = options.directory or "/usr/local/squidguard/db"
 
+available_lists = [db_name for db_name in os.listdir(directory) if "domains" in os.listdir(directory+"/"+db_name)]
+
+lists = [i for i in options.lists if i in available_lists] or []
+whitelists = [i for i in options.whitelists if i in available_lists] or []
+#lists = [i for i in lists if i in available_lists]
+#whitelists = [i for i in whitelists if i in available_lists]
 with open(filename, 'w') as f:
-	f.write("dbhome "+directory)
+	f.write("dbhome "+directory+"\n")
 	f.write("logdir /usr/local/squidGuard/logs\n")
 	f.write("\n")
 	for lista in lists+whitelists:
-		subprocess.call(["cp","-r", "/home/bufferadmin/blacklists/"+lista, directory])
 		f.write("dest "+lista+" {\n")
 		f.write("  domainlist "+lista+"/domains\n")
 		f.write("  urllist "+lista+"/urls\n")
@@ -29,7 +33,7 @@ with open(filename, 'w') as f:
 	f.write("\n")
 	f.write("acl {\n")
 	f.write("  default {\n")
-	f.write("    pass !"+" !".join(lists)+" "+" ".join(whitelists)+" all\n")
+	f.write("    pass "+" ".join(whitelists)+" !".join([""]+lists)+" all\n")
 	f.write("    redirect http://localhost/block.html\n")
 	f.write("  }\n")
 	f.write("}\n")
